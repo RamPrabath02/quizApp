a44c81d5871d456fb8c9fc1e6e81fa2f79a93fd0
Vivin D <vivin.deena@gmail.com>
2024-09-17 11:19:17 +0530

    frontend refactor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-import App2 from "./App2.jsx";
+import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import Home from "./Home.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App2 />
-    {/* <Home /> */}
+    <App />
   </StrictMode>
 );
